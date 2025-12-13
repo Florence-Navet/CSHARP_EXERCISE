cc90fbe3018d2d3ecb8dd5833afb0959ad140f40
Florence <enolaworld@gmail.com>
2025-12-13 18:40:10 +0100

    feat: add method aficher date debut saison

---
+++ CalendrierPerpetuel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace CalendrierPerpetuel;
+
+internal class Program
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("Hello, Calendrier Perpertuel !");
+
+
+      CalculateurCalendrier.AfficherDatesDebutsSaisons(2025);
+
+      Console.WriteLine("***************************************************************");
+
+      CalculateurCalendrier.CalculerJoursFériésFrançais(2025);
+
+
+
+
+
+
+
+
+
+
+   }
+}
