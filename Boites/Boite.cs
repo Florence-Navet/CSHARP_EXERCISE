352554ddc6c5cbfa60aeff69b763e502b007939e
Florence <enolaworld@gmail.com>
2025-12-30 12:40:30 +0100

    feat: exercice boite

---
+++ Boites/Boite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boites
+{
+   public enum Matieres { Carton, Plastique, Bois, Metal }
+   internal class Boite
+   {
+      #region Propriétes en lecture seule
+      public double Hauteur { get; } = 30;
+      public double Largeur { get; } = 30;
+      public double Longueur { get; } = 30;
+      public double Volume => Hauteur * Largeur * Longueur;
+
+      public Matieres Matiere { get; } = Matieres.Carton;
+
+      public string Destinataire { get; private set; } = string.Empty;
+
+      public bool Fragile { get; private set; }
+
+
+      #endregion
+
+      #region Methode publique
+
+      /// <summary>
+      /// permet d'affecter la valeur de la prop du destinataire
+      /// </summary>
+      public void Etiqueter(string dest)
+      {
+         Destinataire = dest;
+       
+
+      }
+
+      public void Etiqueter(string dest, bool f)
+      {
+         Etiqueter(dest);
+         Fragile = f;
+         
+      }
+      #endregion
+   }
+}
---
+++ Boites/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Boites
+{
+   using System;
+   using System.Globalization;
+   using System.IO;
+
+   internal static class Program
+   {
+      static void Main(string[] args)
+      {
+         
+         Console.WriteLine("Hello Boites");
+
+         
+
+         Boite b1= new Boite();
+
+         Console.WriteLine($"Boite de volume {b1.Volume} en {b1.Matiere} ");
+
+
+         b1.Etiqueter("M.Blabla", false);
+         Console.WriteLine($"Boite de {b1.Destinataire} {(b1.Fragile ? "Fragile" : "Non Fragile" )} ");
+        
+
+       
+      }
+
+   }
+    
+}
