15a7e6290aea0ef8d1fded0a7e97d3747de61578
NAVET Florence <fnavet@nge.fr>
2026-01-06 10:48:02 +0100

    feat: exercice modeledocs

---
+++ ModelesDocs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelesDocs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
